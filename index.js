d715ed9ad433a9318245994f1300bd22afbaa43d
DG Labs <vuralokcu@yandex.com>
2025-10-08 02:18:08 +0300

    formats test

--- index.js
+++ index.js
@@ -18,11 +18,33 @@ app.post("/webhook", async (req, res) => {
     if (text === "/start") {
         await telegram.sendMessage(chatId, "👋 Hello! I'm BaseBot. Type /help for help.");
     } else if (text === "/help") {
-        await telegram.sendMessage(chatId, "📖 Commands:\n/start - Start\n/help - Help\n/format - Formatted Text Message\n/img - Image with text");
+        await telegram.sendMessage(chatId, "📖 Commands:\n/start - Start\n/help - Help\n/format - " +
+            "Formatted Text Message\n/img - Image with text\n/keyboard - Keyboard\n/inline_keyboard - Inline Keyboard");
     } else if (text === "/format") {
         await telegram.sendMessage(chatId, "<b>Bold</b> <i>Italic</i> <a href='https://google.com'>Link</a> <code>inline code</code>", { parse_mode: "HTML"});
     } else if (text === "/img") {
         await telegram.sendPhoto(chatId, "https://pbs.twimg.com/profile_images/1966633565988196352/OHLHd5Im_400x400.jpg", "Caption is here");
+    } else if (text === "/keyboard") {
+        await telegram.sendMessage(chatId, "Bir seçenek seç 👇", { reply_markup: {
+                keyboard: [
+                    [{ text: "🚀 Start" }, { text: "ℹ️ Info" }],
+                    [{ text: "❌ Cancel" }]
+                ],
+                resize_keyboard: true,
+                one_time_keyboard: true
+            }});
+    }  else if (text === "/inline_keyboard") {
+        await telegram.sendMessage(chatId, "Seçenekler:", { reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "🌐 Website", url: "https://alvion.io" },
+                        { text: "📊 Stats", callback_data: "stats" }
+                    ],
+                    [
+                        { text: "⚙️ Settings", callback_data: "settings" }
+                    ]
+                ]
+            }});
     } else {
         await telegram.sendMessage(chatId, "❓ I didn't recognize the command. You can type /help.");
     }
